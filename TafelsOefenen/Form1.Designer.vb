b0426bcf246625ce52420fc6be75e12e6f71c534
Jan De Clerck <briel@qualident.be>
2022-09-24 20:26:21 +0200

    Add project files.

---
+++ TafelsOefenen/Form1.Designer.vb
@@ -0,0 +1,263 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.StartBtn = New System.Windows.Forms.Button()
+        Me.OpdrachtLbl = New System.Windows.Forms.Label()
+        Me.AntwoordTxt = New System.Windows.Forms.TextBox()
+        Me.Timer = New System.Windows.Forms.Timer(Me.components)
+        Me.Tijdprg = New System.Windows.Forms.ProgressBar()
+        Me.ScoreLbl = New System.Windows.Forms.Label()
+        Me.WillekeurigOptBtn = New System.Windows.Forms.RadioButton()
+        Me.LaagsteScoreOptbtn = New System.Windows.Forms.RadioButton()
+        Me.MinstGeoefendOptBtn = New System.Windows.Forms.RadioButton()
+        Me.AantalPerSessieTxt = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.InstellingenPanel = New System.Windows.Forms.Panel()
+        Me.StopBtn = New System.Windows.Forms.Button()
+        Me.StatusOefeningLbl = New System.Windows.Forms.Label()
+        Me.dgv = New System.Windows.Forms.DataGridView()
+        Me.RecordLbl = New System.Windows.Forms.Label()
+        Me.NogNietGeoefendLbl = New System.Windows.Forms.Label()
+        Me.AllesWissenBtn = New System.Windows.Forms.Button()
+        Me.InstellingenPanel.SuspendLayout()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'StartBtn
+        '
+        Me.StartBtn.Location = New System.Drawing.Point(21, 38)
+        Me.StartBtn.Name = "StartBtn"
+        Me.StartBtn.Size = New System.Drawing.Size(207, 58)
+        Me.StartBtn.TabIndex = 0
+        Me.StartBtn.Text = "Starten"
+        Me.StartBtn.UseVisualStyleBackColor = True
+        '
+        'OpdrachtLbl
+        '
+        Me.OpdrachtLbl.AutoSize = True
+        Me.OpdrachtLbl.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.OpdrachtLbl.Location = New System.Drawing.Point(20, 250)
+        Me.OpdrachtLbl.Name = "OpdrachtLbl"
+        Me.OpdrachtLbl.Size = New System.Drawing.Size(0, 54)
+        Me.OpdrachtLbl.TabIndex = 1
+        '
+        'AntwoordTxt
+        '
+        Me.AntwoordTxt.Enabled = False
+        Me.AntwoordTxt.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.AntwoordTxt.Location = New System.Drawing.Point(199, 250)
+        Me.AntwoordTxt.Name = "AntwoordTxt"
+        Me.AntwoordTxt.Size = New System.Drawing.Size(107, 61)
+        Me.AntwoordTxt.TabIndex = 2
+        '
+        'Timer
+        '
+        Me.Timer.Interval = 1000
+        '
+        'Tijdprg
+        '
+        Me.Tijdprg.Location = New System.Drawing.Point(349, 267)
+        Me.Tijdprg.Maximum = 60
+        Me.Tijdprg.Name = "Tijdprg"
+        Me.Tijdprg.Size = New System.Drawing.Size(342, 35)
+        Me.Tijdprg.TabIndex = 4
+        '
+        'ScoreLbl
+        '
+        Me.ScoreLbl.AutoSize = True
+        Me.ScoreLbl.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.ScoreLbl.ForeColor = System.Drawing.Color.Blue
+        Me.ScoreLbl.Location = New System.Drawing.Point(21, 338)
+        Me.ScoreLbl.Name = "ScoreLbl"
+        Me.ScoreLbl.Size = New System.Drawing.Size(171, 54)
+        Me.ScoreLbl.TabIndex = 5
+        Me.ScoreLbl.Text = "Score: 0"
+        '
+        'WillekeurigOptBtn
+        '
+        Me.WillekeurigOptBtn.AutoSize = True
+        Me.WillekeurigOptBtn.Location = New System.Drawing.Point(23, 22)
+        Me.WillekeurigOptBtn.Name = "WillekeurigOptBtn"
+        Me.WillekeurigOptBtn.Size = New System.Drawing.Size(205, 29)
+        Me.WillekeurigOptBtn.TabIndex = 6
+        Me.WillekeurigOptBtn.Text = "Willekeurig opvragen"
+        Me.WillekeurigOptBtn.UseVisualStyleBackColor = True
+        '
+        'LaagsteScoreOptbtn
+        '
+        Me.LaagsteScoreOptbtn.AutoSize = True
+        Me.LaagsteScoreOptbtn.Checked = True
+        Me.LaagsteScoreOptbtn.Location = New System.Drawing.Point(23, 57)
+        Me.LaagsteScoreOptbtn.Name = "LaagsteScoreOptbtn"
+        Me.LaagsteScoreOptbtn.Size = New System.Drawing.Size(346, 29)
+        Me.LaagsteScoreOptbtn.TabIndex = 7
+        Me.LaagsteScoreOptbtn.TabStop = True
+        Me.LaagsteScoreOptbtn.Text = "Opvragen waar ik het moeilijk mee heb"
+        Me.LaagsteScoreOptbtn.UseVisualStyleBackColor = True
+        '
+        'MinstGeoefendOptBtn
+        '
+        Me.MinstGeoefendOptBtn.AutoSize = True
+        Me.MinstGeoefendOptBtn.Location = New System.Drawing.Point(23, 92)
+        Me.MinstGeoefendOptBtn.Name = "MinstGeoefendOptBtn"
+        Me.MinstGeoefendOptBtn.Size = New System.Drawing.Size(299, 29)
+        Me.MinstGeoefendOptBtn.TabIndex = 8
+        Me.MinstGeoefendOptBtn.Text = "Minst geoefende tafels opvragen"
+        Me.MinstGeoefendOptBtn.UseVisualStyleBackColor = True
+        '
+        'AantalPerSessieTxt
+        '
+        Me.AantalPerSessieTxt.Location = New System.Drawing.Point(235, 137)
+        Me.AantalPerSessieTxt.Name = "AantalPerSessieTxt"
+        Me.AantalPerSessieTxt.Size = New System.Drawing.Size(67, 31)
+        Me.AantalPerSessieTxt.TabIndex = 10
+        Me.AantalPerSessieTxt.Text = "10"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(23, 140)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(194, 25)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Aantal Per Oefensessie:"
+        '
+        'InstellingenPanel
+        '
+        Me.InstellingenPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.InstellingenPanel.Controls.Add(Me.WillekeurigOptBtn)
+        Me.InstellingenPanel.Controls.Add(Me.Label1)
+        Me.InstellingenPanel.Controls.Add(Me.LaagsteScoreOptbtn)
+        Me.InstellingenPanel.Controls.Add(Me.AantalPerSessieTxt)
+        Me.InstellingenPanel.Controls.Add(Me.MinstGeoefendOptBtn)
+        Me.InstellingenPanel.Location = New System.Drawing.Point(271, 38)
+        Me.InstellingenPanel.Name = "InstellingenPanel"
+        Me.InstellingenPanel.Size = New System.Drawing.Size(386, 187)
+        Me.InstellingenPanel.TabIndex = 12
+        '
+        'StopBtn
+        '
+        Me.StopBtn.Enabled = False
+        Me.StopBtn.Location = New System.Drawing.Point(21, 161)
+        Me.StopBtn.Name = "StopBtn"
+        Me.StopBtn.Size = New System.Drawing.Size(207, 58)
+        Me.StopBtn.TabIndex = 13
+        Me.StopBtn.Text = "Stoppen"
+        Me.StopBtn.UseVisualStyleBackColor = True
+        '
+        'StatusOefeningLbl
+        '
+        Me.StatusOefeningLbl.AutoSize = True
+        Me.StatusOefeningLbl.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.StatusOefeningLbl.Location = New System.Drawing.Point(21, 114)
+        Me.StatusOefeningLbl.Name = "StatusOefeningLbl"
+        Me.StatusOefeningLbl.Size = New System.Drawing.Size(179, 32)
+        Me.StatusOefeningLbl.TabIndex = 14
+        Me.StatusOefeningLbl.Text = "Oefening 0/10"
+        '
+        'dgv
+        '
+        Me.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv.Location = New System.Drawing.Point(21, 548)
+        Me.dgv.Name = "dgv"
+        Me.dgv.RowHeadersWidth = 62
+        Me.dgv.RowTemplate.Height = 33
+        Me.dgv.Size = New System.Drawing.Size(727, 553)
+        Me.dgv.TabIndex = 15
+        '
+        'RecordLbl
+        '
+        Me.RecordLbl.AutoSize = True
+        Me.RecordLbl.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.RecordLbl.Location = New System.Drawing.Point(590, 355)
+        Me.RecordLbl.Name = "RecordLbl"
+        Me.RecordLbl.Size = New System.Drawing.Size(101, 32)
+        Me.RecordLbl.TabIndex = 16
+        Me.RecordLbl.Text = "Record:"
+        Me.RecordLbl.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'NogNietGeoefendLbl
+        '
+        Me.NogNietGeoefendLbl.AutoSize = True
+        Me.NogNietGeoefendLbl.Location = New System.Drawing.Point(315, 388)
+        Me.NogNietGeoefendLbl.Name = "NogNietGeoefendLbl"
+        Me.NogNietGeoefendLbl.Size = New System.Drawing.Size(0, 25)
+        Me.NogNietGeoefendLbl.TabIndex = 17
+        '
+        'AllesWissenBtn
+        '
+        Me.AllesWissenBtn.Location = New System.Drawing.Point(21, 437)
+        Me.AllesWissenBtn.Name = "AllesWissenBtn"
+        Me.AllesWissenBtn.Size = New System.Drawing.Size(131, 43)
+        Me.AllesWissenBtn.TabIndex = 18
+        Me.AllesWissenBtn.Text = "Alles Wissen"
+        Me.AllesWissenBtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(760, 434)
+        Me.Controls.Add(Me.AllesWissenBtn)
+        Me.Controls.Add(Me.NogNietGeoefendLbl)
+        Me.Controls.Add(Me.RecordLbl)
+        Me.Controls.Add(Me.dgv)
+        Me.Controls.Add(Me.StatusOefeningLbl)
+        Me.Controls.Add(Me.StopBtn)
+        Me.Controls.Add(Me.InstellingenPanel)
+        Me.Controls.Add(Me.ScoreLbl)
+        Me.Controls.Add(Me.Tijdprg)
+        Me.Controls.Add(Me.AntwoordTxt)
+        Me.Controls.Add(Me.OpdrachtLbl)
+        Me.Controls.Add(Me.StartBtn)
+        Me.Name = "Form1"
+        Me.Text = "Tafels Oefenen "
+        Me.InstellingenPanel.ResumeLayout(False)
+        Me.InstellingenPanel.PerformLayout()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents StartBtn As Button
+    Friend WithEvents OpdrachtLbl As Label
+    Friend WithEvents AntwoordTxt As TextBox
+    Friend WithEvents Timer As Timer
+    Friend WithEvents Tijdprg As ProgressBar
+    Friend WithEvents ScoreLbl As Label
+    Friend WithEvents WillekeurigOptBtn As RadioButton
+    Friend WithEvents LaagsteScoreOptbtn As RadioButton
+    Friend WithEvents MinstGeoefendOptBtn As RadioButton
+    Friend WithEvents AantalPerSessieTxt As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents InstellingenPanel As Panel
+    Friend WithEvents StopBtn As Button
+    Friend WithEvents StatusOefeningLbl As Label
+    Friend WithEvents dgv As DataGridView
+    Friend WithEvents RecordLbl As Label
+    Friend WithEvents NogNietGeoefendLbl As Label
+    Friend WithEvents AllesWissenBtn As Button
+End Class
---
+++ TafelsOefenen/Form1.vb
@@ -0,0 +1,291 @@
+﻿Imports System.ComponentModel
+
+Public Class Form1
+    Private Factor1 As Integer = 1
+    Private Factor2 As Integer = 1
+    Private Pogingen As Integer = 0
+    Private Score As Integer = 0
+    Private Dt As DataTable
+    Private GemiddeldeDt As DataTable
+    Private AantalPerSessie As Integer = 10
+    Private OefeningNummer As Integer
+    Private OefeningBezig As Boolean = False
+    Private LaagsteScoreDt As DataTable
+    Private MinstGeoefendDt As DataTable
+    Private FormLoadFinisched As Boolean = False
+    Private AantalNogNietGeoefend As Integer = 81
+
+
+    Private Enum TafelModi
+        Willekeurig = 1
+        LaagsteScore = 2
+        MinstGeoefend = 3
+    End Enum
+    Private TafelModus As TafelModi
+
+    Private Sub MaakLaagsteScoreDt()
+        Dim i As Integer
+        Dim TopDr() As DataRow = GemiddeldeDt.Select("Aantalkeer > 0", "Score ASC")
+        If TopDr.Length < AantalPerSessie Then
+            TopDr = GemiddeldeDt.Select("", "Score ASC")
+        End If
+        LaagsteScoreDt = GemiddeldeDt.Clone
+        LaagsteScoreDt.Clear()
+        For i = 0 To AantalPerSessie - 1
+            LaagsteScoreDt.ImportRow(TopDr(i))
+        Next
+        dgv.DataSource = LaagsteScoreDt
+    End Sub
+    Private Sub MaakMinstGeoeffendDt()
+        GemiddeldeDt.DefaultView.Sort = "AantalKeer ASC"
+        MinstGeoefendDt = GemiddeldeDt.DefaultView.ToTable.AsEnumerable().Take(AantalPerSessie).CopyToDataTable
+        dgv.DataSource = MinstGeoefendDt
+    End Sub
+
+    Private Sub NieuweOpgave()
+        OefeningNummer += 1
+        If OefeningNummer > AantalPerSessie Then
+            StopBtn_Click(Nothing, Nothing)
+            Exit Sub
+        End If
+
+        Select Case TafelModus
+            Case TafelModi.Willekeurig
+                Dim Generator As System.Random = New System.Random
+                Factor1 = Generator.Next(1, 9)
+                Factor2 = Generator.Next(1, 9)
+            Case TafelModi.LaagsteScore
+                Dim Dr As DataRow
+                Dr = LaagsteScoreDt.Rows.Item(OefeningNummer - 1)
+                Factor1 = Dr.Item("Factor1")
+                Factor2 = Dr.Item("Factor2")
+            Case TafelModi.MinstGeoefend
+                Dim Dr As DataRow
+                Dr = MinstGeoefendDt.Rows.Item(OefeningNummer - 1)
+                Factor1 = Dr.Item("Factor1")
+                Factor2 = Dr.Item("Factor2")
+        End Select
+
+        Pogingen = 0
+        ToonOpgave()
+    End Sub
+
+    Private Sub ToonOpgave()
+        OpdrachtLbl.Text = Factor1.ToString + " x " + Factor2.ToString + " ="
+        Tijdprg.Value = 0
+        Timer.Enabled = True
+        AntwoordTxt.Text = ""
+        AntwoordTxt.Focus()
+        ScoreLbl.Text = "Score: " + Score.ToString
+        StatusOefeningLbl.Text = "Oefening " + OefeningNummer.ToString + "/" + AantalPerSessie.ToString
+
+    End Sub
+    Private Sub AntwoordTxt_KeyDown(sender As Object, e As KeyEventArgs) Handles AntwoordTxt.KeyDown
+        If e.KeyValue = Keys.Enter Then
+
+            If Val(AntwoordTxt.Text) = Factor1 * Factor2 Then
+                RegistreerJuistAntwoord()
+            Else
+                FoutAntwoord()
+            End If
+            e.Handled = True
+            e.SuppressKeyPress = True
+        End If
+    End Sub
+    Private Sub RegistreerJuistAntwoord()
+        My.Computer.Audio.Play(My.Resources.ok, AudioPlayMode.Background)
+        Dim BehaaldeScore As Integer = Int((60 - Tijdprg.Value) / (Pogingen + 1))
+        Timer.Enabled = False
+        Score += BehaaldeScore
+        ScoreLbl.Text = "Score: " + Score.ToString
+        'gegevens opslaan
+        Dt.Rows.Add({Factor1, Factor2, Pogingen, BehaaldeScore, Now})
+        NieuweOpgave()
+
+
+
+    End Sub
+    Private Sub FoutAntwoord()
+        Beep()
+        Pogingen = Pogingen + 1
+        AntwoordTxt.SelectAll()
+
+
+    End Sub
+    Public Sub GeenAntwoord()
+        Beep()
+        Tijdprg.Value = 0
+        Dt.Rows.Add({Factor1, Factor2, Pogingen, 0, Now})
+        NieuweOpgave()
+    End Sub
+    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
+        If Tijdprg.Value = 60 Then GeenAntwoord()
+        Tijdprg.Value = Tijdprg.Value + 1
+    End Sub
+    Private Sub MaakOefeningDatatable()
+        BerekenGemiddeldes()
+        Select Case TafelModus
+            Case TafelModi.LaagsteScore
+                MaakLaagsteScoreDt()
+            Case TafelModi.MinstGeoefend
+                MaakMinstGeoeffendDt()
+        End Select
+    End Sub
+    Private Sub StartBtn_Click(sender As Object, e As EventArgs) Handles StartBtn.Click
+        AntwoordTxt.Enabled = True
+        StartBtn.Enabled = False
+        StopBtn.Enabled = True
+        Score = 0
+        OefeningNummer = 0
+        InstellingenPanel.Enabled = False
+        OefeningBezig = True
+        NieuweOpgave()
+        Timer.Enabled = True
+    End Sub
+
+    Private Sub StopBtn_Click(sender As Object, e As EventArgs) Handles StopBtn.Click
+        InstellingenPanel.Enabled = True
+        StartBtn.Enabled = True
+        StopBtn.Enabled = False
+        OefeningBezig = False
+        If Score > (My.Settings.HighScore) Then
+            MsgBox("Proficiat, je verbeterde je record! Vorig record:" + My.Settings.HighScore.ToString + " Nieuw record: " + Score.ToString)
+            My.Settings.HighScore = Score
+            RecordLbl.Text = "Record: " + My.Settings.HighScore.ToString
+        End If
+        OefeningNummer = 0
+        Score = 0
+        Timer.Enabled = False
+        StatusOefeningLbl.Text = "Klik op Start"
+        MaakOefeningDatatable()
+
+        OpdrachtLbl.Text = ""
+        AntwoordTxt.Text = ""
+        AntwoordTxt.Enabled = False
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        RecordLbl.Text = "Record: " + My.Settings.HighScore.ToString
+        AantalPerSessieTxt.Text = AantalPerSessie.ToString
+        If System.IO.File.Exists("Tafels.xml") Then
+            Dt = New DataTable
+            Dt.ReadXml("Tafels.xml")
+        Else
+            MaakDt()
+        End If
+        If System.IO.File.Exists("GemiddeldeTafels.xml") Then
+            GemiddeldeDt = New DataTable
+            GemiddeldeDt.ReadXml("GemiddeldeTafels.xml")
+        Else
+
+            MaakGemiddeldeDt()
+        End If
+        BerekenGemiddeldes()
+        MaakOefeningDatatable()
+        FormLoadFinisched = True
+
+    End Sub
+    Private Sub MaakDt()
+        Dt = New DataTable
+        With Dt
+            .TableName = "Tafels"
+            .Columns.Add(New DataColumn("Factor1", GetType(System.String)))
+            .Columns.Add(New DataColumn("Factor2", GetType(System.String)))
+            .Columns.Add(New DataColumn("Pogingen", GetType(System.Int16)))
+            .Columns.Add(New DataColumn("Score", GetType(System.Int16)))
+            .Columns.Add(New DataColumn("Datum", GetType(System.DateTime)))
+            '.Rows.Add({1, 1, 0, 0, Now})
+            .WriteXml("Tafels.xml", XmlWriteMode.WriteSchema)
+        End With
+    End Sub
+    Private Sub MaakGemiddeldeDt()
+        GemiddeldeDt = New DataTable
+        With GemiddeldeDt
+            .TableName = "Tafels"
+            .Columns.Add(New DataColumn("Factor1", GetType(System.String)))
+            .Columns.Add(New DataColumn("Factor2", GetType(System.String)))
+            .Columns.Add(New DataColumn("Pogingen", GetType(System.Int16)))
+            .Columns.Add(New DataColumn("Score", GetType(System.Single)))
+            .Columns.Add(New DataColumn("AantalKeer", GetType(System.Int16)))
+            .WriteXml("GemiddeldeTafels.xml", XmlWriteMode.WriteSchema)
+        End With
+    End Sub
+    Private Sub BerekenGemiddeldes()
+        Dim i As Integer
+        Dim j As Integer
+        Dim RowsFound() As DataRow
+        Dim GemPogingen As Single
+        Dim GemScore As Integer
+        '  If IsNothing(Dt) Then MaakDt()
+        ' If IsNothing(GemiddeldeDt) Then MaakGemiddeldeDt()
+        AantalNogNietGeoefend = 0
+        GemiddeldeDt.Rows.Clear()
+
+        For i = 1 To 9
+            For j = 1 To 9
+                RowsFound = Dt.Select("Factor1 = " + i.ToString + " AND Factor2 = " + j.ToString)
+                GemPogingen = 0
+                GemScore = 0
+                For Each Dr As DataRow In RowsFound
+                    GemPogingen += Dr.Item("Pogingen")
+                    GemScore += Dr.Item("Score")
+                Next
+                If RowsFound.Count <> 0 Then
+                    GemPogingen = GemPogingen / RowsFound.Count
+                    GemScore = GemScore / RowsFound.Count
+                    GemiddeldeDt.Rows.Add({i, j, GemPogingen, GemScore, RowsFound.Count})
+                Else
+                    AantalNogNietGeoefend += 1
+                    GemiddeldeDt.Rows.Add({i, j, 0, 0, 0})
+                End If
+
+            Next
+            Next
+        GemiddeldeDt.WriteXml("GemiddeldeTafels.xml", XmlWriteMode.WriteSchema)
+        If AantalNogNietGeoefend <> 0 Then NogNietGeoefendLbl.Text = AantalNogNietGeoefend.ToString + " nog niet geoefende tafels"
+    End Sub
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        Dt.WriteXml("Tafels.xml", XmlWriteMode.WriteSchema)
+        BerekenGemiddeldes 
+        GemiddeldeDt.WriteXml("GemiddeldeTafels.xml", XmlWriteMode.WriteSchema)
+    End Sub
+
+
+    Private Sub ModusChanged(sender As Object, e As EventArgs) Handles LaagsteScoreOptbtn.CheckedChanged, MinstGeoefendOptBtn.CheckedChanged, WillekeurigOptBtn.CheckedChanged
+        If WillekeurigOptBtn.Checked Then TafelModus = TafelModi.Willekeurig
+        If LaagsteScoreOptbtn.Checked Then TafelModus = TafelModi.LaagsteScore
+        If MinstGeoefendOptBtn.Checked Then TafelModus = TafelModi.MinstGeoefend
+        If FormLoadFinisched Then MaakOefeningDatatable()
+    End Sub
+
+
+
+    Private Sub AantalPerSessieTxt_LostFocus(sender As Object, e As EventArgs) Handles AantalPerSessieTxt.LostFocus
+        AantalPerSessie = Val(AantalPerSessieTxt.Text)
+        AantalPerSessieTxt.Text = AantalPerSessie.ToString
+    End Sub
+
+    Private Sub AntwoordTxt_TextChanged(sender As Object, e As EventArgs) Handles AntwoordTxt.TextChanged
+
+    End Sub
+
+    Private Sub AllesWissenBtn_Click(sender As Object, e As EventArgs) Handles AllesWissenBtn.Click
+        Dim Antwoord As MsgBoxResult = MsgBox("Bent u zeker dat alles gewist mag worden", vbYesNoCancel)
+        Select Case Antwoord
+            Case MsgBoxResult.No Or MsgBoxResult.Cancel
+                ' niets doen
+            Case MsgBoxResult.Yes
+
+                Try
+                    My.Computer.FileSystem.DeleteFile("Tafels.xml")
+                    My.Computer.FileSystem.DeleteFile("GemiddeldeTafels.xml")
+                    My.Settings.HighScore = 0
+                    ScoreLbl.Text = "Score: 0"
+                    Form1_Load(Nothing, Nothing)
+
+                Catch ex As Exception
+                    MsgBox(ex.ToString)
+                End Try
+        End Select
+    End Sub
+End Class
